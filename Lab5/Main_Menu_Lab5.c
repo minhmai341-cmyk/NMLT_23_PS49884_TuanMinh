5e197ad7dbdd78894d1b9595a467ea5afbec7e98
minhmai341-cmyk <minhmai341@gmail.com>
2026-08-06 10:09:09 +0700

    Lab5: Dung khung Menu va khai bao cac nguyen mau ham (Function Prototypes)

---
+++ Lab5/Main_Menu_Lab5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void menu();
+int main(){
+    int chon;
+    do
+    {
+        menu();
+        printf(">> Xin moi chon chuc nang (1-5): ");
+        scanf("%d",&chon);
+        switch (chon)
+        {
+        case 5:
+            printf("Tam biet!");
+            break;
+        default:
+            printf("Ban phai chon chuc nang tu 1 - 5\n");
+            break;
+        }
+    } while (chon != 5);
+    
+    return 0;
+}
+void menu(){
+    printf("+---------------------------------------------------+\n");
+    printf("|             MENU CHUONG TRINH LAB 5               |\n");
+    printf("+---------------------------------------------------+\n");
+    printf("| 1. Tim gia tri lon nhat trong 3 so                |\n");
+    printf("| 2. Kiem tra Nam nhuan                             |\n");
+    printf("| 3. Hoan vi 2 so (Su dung Con tro)                 |\n");
+    printf("| 4. Kiem tra & Phan loai Tam giac                  |\n");
+    printf("| 5. Thoat chuong trinh                             |\n");
+    printf("+---------------------------------------------------+\n");
+}
